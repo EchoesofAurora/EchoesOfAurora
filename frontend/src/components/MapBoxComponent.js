84fa58a5ad908e41d2bc3c42c7402fc43282b180
Taegan Maishman <taegandm@gmail.com>
2025-04-09 12:07:54 -0400

    Fix Timeline Position (wouldn't update from pull request)

--- frontend/src/components/MapBoxComponent.js
+++ frontend/src/components/MapBoxComponent.js
@@ -262,9 +262,9 @@ const MapBoxComponent = () => {
         return {
           position: "fixed", 
           bottom: 20,      
-          right: 350,       // Adjusted based on side panel width
+          right: 50,       // Adjusted based on side panel width
           left: 'auto',
-          width: "40%",
+          width: "50%",
           zIndex: 1000     // Ensure it's above map but below other controls
         };
       }
